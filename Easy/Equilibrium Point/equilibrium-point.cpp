0dfccbfe0a070b3a2d5176d2d1ca0294d89d664d
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-04-16 00:25:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Equilibrium Point/equilibrium-point.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        int right_sum = 0;
+        int left_sum = 0;
+        
+        
+        for(int i = 0; i < n; i++)
+        {
+            right_sum += a[i];
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            
+            right_sum -= a[i];
+
+            if (left_sum == right_sum)
+            {
+              return i+1;  
+            }
+             
+            left_sum += a[i];
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
