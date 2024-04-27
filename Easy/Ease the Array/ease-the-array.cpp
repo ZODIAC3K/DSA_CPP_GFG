5993cb442f5b73cb72bd19a00e99843f92373175
Harsh Deepanshu <88622014+ZODIAC3K@users.noreply.github.com>
2024-04-28 01:55:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Ease the Array/ease-the-array.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+// C++ implementation to rearrange the array 
+// elements after modification 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        void modifyAndRearrangeArr(int arr[], int n) 
+        {
+    	    int index = 0 ;
+            for(int i = 0 ; i < n - 1 ; i++) 
+                if(arr[i] != 0 && arr[i] == arr[i + 1])
+                {
+                    arr[i] *= 2 ;
+                    arr[i + 1] = 0 ;
+                }
+                
+            for(int i = 0 ; i < n ; i++) 
+                if(arr[i] != 0)
+                    arr[index++] = arr[i] ;
+                    
+            while(index < n) 
+                arr[index++] = 0 ;
+    
+        }
+}; 
+
+
+//{ Driver Code Starts.
+// Driver program to test above 
+int main() 
+{ 
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        ob.modifyAndRearrangeArr(arr, n);
+        for(int i=0;i<n;i++)
+            cout << arr[i] << " ";
+        cout << endl;
+        
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
